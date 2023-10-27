e1fb811f23494b59967ae54b2d48e0b00f566728
QobiljonIbaydullayevDev <qobiljonyoqubovich2002@gmail.com>
2023-10-27 23:25:53 +0500

    Added hours, minutes and seconds

---
+++ script.js
@@ -0,0 +1,31 @@
+const numberHours = document.querySelector('.number-hours');
+const barSeconds =document.querySelector('.bar-seconds');
+
+const numberElements = [];
+const barElements=[]
+
+for(let i=1; i<=12; i++){
+    numberElements.push(
+        `<span style="--index:${i}">
+        <p>
+        ${i}
+        </p>
+        </span>`
+    )
+}
+
+numberHours.insertAdjacentHTML("afterbegin", numberElements.join(""));
+console.log(numberElements);
+
+for(let i=1; i<=60; i++){
+    barElements.push(
+        `<span style="--index:${i}">
+        <p>
+        </p>
+        </span>`
+    )
+}
+
+barSeconds.insertAdjacentHTML("afterbegin", barElements.join(""));
+console.log(barElements);
+
